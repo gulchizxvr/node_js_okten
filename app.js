1b8d595ab845f1a952f1e710cb5ce45158fe1e98
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-11-27 22:53:31 +0200

    K

--- app.js
+++ app.js
@@ -1,17 +1,66 @@
 const express = require('express')
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
-app.listen(5000, ()=> {
+const userDB = require('./database/users')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const Joi = require('joi')
+const user = Joi.object({
+    name: Joi.string().pattern(new RegExp('^[A-Z]{1,30}$')).required(),
+    age: Joi.number().min(1).required()
+})
+
+
+app.listen(5000, () => {
     console.log('Listen port 5000')
 })
- app.get('/', (req, res) => {
-     res.json("ok")
- })
-app.post('/users', (req, res)=> {
-    const userInfo = req.body
-    console.log(userInfo);
-    res.json("Created")
+
+app.get('/', (req, res) => {
+    res.json("Welcome")
 })
+
+
+app.get('/users', (req, res) => {
+    res.json(userDB)
+})
+
+app.get('/user/:id', (req, res) => {
+    const {id} = req.params
+
+    if (id < userDB.length) {
+        res.json(userDB[id])
+    } else {
+        res.json("No find")
+    }
+
+})
+
+app.post('/users', (req, res) => {
+
+    const userInfo = req.body
+    const {error,value} = user.validate(userInfo)
+
+    if (!error) {
+        userDB.push(value)
+        res.json(`Created user ${value.name}`)
+    } else {
+        res.json("enter username and age")
+    }
+})
+
+app.delete('/user/:id', (req, res) => {
+
+    const {id} = req.params
+
+    if (id < userDB.length) {
+        res.json(`Delete user with name ${userDB[id]?.name}`)
+        userDB.splice(id, 1)
+    } else {
+        res.json("Error. Write valid number")
+    }
+})
+
+
